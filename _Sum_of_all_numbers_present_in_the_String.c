026e977e0f9deb0c9a3895dc4b3dc6bf787773e4
KUDIPUDINAVEEN <147488479+KUDIPUDINAVEEN@users.noreply.github.com>
2024-01-04 22:43:02 +0530

    #include<stdio.h> int main(){     char s[1000];     scanf("%[^ ]s",s);     int i,sum=0;     for(i=0;s[i]!=0;i++){         if(s[i]>='1'&&s[i]<='9'){             sum+=s[i]-'0';         }     }     printf("%d",sum); }

---
+++ _Sum_of_all_numbers_present_in_the_String.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char s[1000];
+    scanf("%[^
+]s",s);
+    int i,sum=0;
+    for(i=0;s[i]!=0;i++){
+        if(s[i]>='1'&&s[i]<='9'){
+            sum+=s[i]-'0';
+        }
+    }
+    printf("%d",sum);
+}
